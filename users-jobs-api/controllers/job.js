0f4f7dabfed0617c0058b95cd42b93d129cc11d1
CalvinC44 <calel44jke@gmail.com>
2023-01-26 22:46:44 +0100

    update createJob to require id

--- users-jobs-api/controllers/job.js
+++ users-jobs-api/controllers/job.js
@@ -12,11 +12,12 @@ exports.getAllJobs = (req, res, next) => {
 	});
 };
 
+//function to create a job, require a name and the id of the recruiter
 exports.createJob = (req, res, next) => {
 	if (!req.body) return next(new AppError("No form data found", 404));
-	const values = [req.body.job_name];
+	const values = [req.body.job_name, req.recruiter_id];
 	connection.query(
-		"INSERT INTO jobs (job_name) VALUES(?)",
+		"INSERT INTO jobs (job_name, recruiter_id) VALUES(?, ?)",
 		[values],
 		function (err, data, fields) {
 			if (err) return next(new AppError(err, 500));
@@ -46,40 +47,40 @@ exports.getJob = (req, res, next) => {
 	);
 };
 
-exports.updateJob = (req, res, next) => {
-	if (!req.params.id) {
-		return next(new AppError("No job id found", 404));
-	}
-	if (!req.body) return next(new AppError("No form data found", 404));
-	let query = "UPDATE jobs SET ";
-	let values = [];
-	if (req.body.job_name) {
-		query += "job_name = ?, ";
-		values.push(req.body.job_name);
-	}
-	if (req.body.birthdate) {
-		query += "birthdate = ?, ";
-		values.push(req.body.birthdate);
-	}
-	if (req.body.description) {
-		query += "description = ?, ";
-		values.push(req.body.description);
-	}
-	if (req.body.location) {
-		query += "location = ?, ";
-		values.push(req.body.location);
-	}
-	query = query.slice(0, -2); // Removing the last comma and space
-	query += " WHERE id = ?";
-	values.push(req.params.id);
-	connection.query(query, values, function (err, data, fields) {
-		if (err) return next(new AppError(err, 500));
-		res.status(200).json({
-			status: "success",
-			message: "job updated!"
-		});
-	});
-};
+// exports.updateJob = (req, res, next) => {
+// 	if (!req.params.id) {
+// 		return next(new AppError("No job id found", 404));
+// 	}
+// 	if (!req.body) return next(new AppError("No form data found", 404));
+// 	let query = "UPDATE jobs SET ";
+// 	let values = [];
+// 	if (req.body.job_name) {
+// 		query += "job_name = ?, ";
+// 		values.push(req.body.job_name);
+// 	}
+// 	if (req.body.birthdate) {
+// 		query += "birthdate = ?, ";
+// 		values.push(req.body.birthdate);
+// 	}
+// 	if (req.body.description) {
+// 		query += "description = ?, ";
+// 		values.push(req.body.description);
+// 	}
+// 	if (req.body.location) {
+// 		query += "location = ?, ";
+// 		values.push(req.body.location);
+// 	}
+// 	query = query.slice(0, -2); // Removing the last comma and space
+// 	query += " WHERE id = ?";
+// 	values.push(req.params.id);
+// 	connection.query(query, values, function (err, data, fields) {
+// 		if (err) return next(new AppError(err, 500));
+// 		res.status(200).json({
+// 			status: "success",
+// 			message: "job updated!"
+// 		});
+// 	});
+// };
 
 exports.deleteJob = (req, res, next) => {
 	if (!req.params.id) {
