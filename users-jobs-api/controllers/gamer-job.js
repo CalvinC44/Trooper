dd714f349b72ec5f54d2e8e185a5c5b65bbf959a
CalvinC44 <calel44jke@gmail.com>
2023-01-30 17:50:36 +0100

    gamer-job functions initiate

---
+++ users-jobs-api/controllers/gamer-job.js
@@ -0,0 +1,9 @@
+//function to get total_earned, if null does the request and save it
+
+//function to make a gamer apply for a job
+
+//function to make a gamer accept a job a recruiter asks him
+
+//function for a recruiter to accept an applicant for a job
+
+//function for a recruiter to ask a gamer for a job
