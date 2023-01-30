4250203a70f040d7ecdedff757f5430257ffa908
CalvinC44 <calel44jke@gmail.com>
2023-01-30 17:49:17 +0100

    update : create and update gamer function

--- users-jobs-api/controllers/gamer.js
+++ users-jobs-api/controllers/gamer.js
@@ -1,6 +1,7 @@
 const AppError = require("../utils/appError");
 const connection = require("../services/db");
 
+//function to get all gamers
 exports.getAllGamers = (req, res, next) => {
 	connection.query("SELECT * FROM gamers", function (err, data, fields) {
 		if (err) return next(new AppError(err));
@@ -12,29 +13,64 @@ exports.getAllGamers = (req, res, next) => {
 	});
 };
 
+// function to create gamer, using username and profile_type
 exports.createGamer = (req, res, next) => {
+	if (!req.body) return next(new AppError("No form data found", 404));
 	if (!req.body.username) return next(new AppError("No username found", 404));
+	if (this.checkUserExists(req.body.username))
+		return next(new AppError("Username already exists", 400));
 	if (
 		req.body.profile_type !== "Gamer" &&
 		req.body.profile_type !== "Recruiter" &&
 		req.body.profile_type !== "Guild Manager"
 	)
 		return next(new AppError("Incorrect type of profile", 400));
-	const values = [req.body.username, req.body.profile_type];
-	connection.query(
-		"INSERT INTO gamers (username, profile_type) VALUES(?,?)",
-		values,
-		function (err, result) {
-			if (err) return next(new AppError(err, 500));
-			res.status(201).json({
-				status: "success",
-				message: "gamer created!",
-				data: result
-			});
+	if (req.body.min_hour_rate && req.body.min_hour_rate <= 0)
+		return next(new AppError("Wrong minimum hour rate value", 400));
+	if (req.body.hours_per_day >= 24 && req.body.hours_per_day <= 0)
+		return next(new AppError("Wrong hours_per_day hour rate value", 400));
+
+	//initiate query and values, username and profile_type are required
+	let query = "INSERT INTO gamers (username, profile_type";
+	let values = [req.body.username, req.body.profile_type];
+
+	//possibility to set other attributes of the gamer
+	const columnMap = {
+		location: "location",
+		birthdate: "birthdate",
+		description: "description",
+		name_discord: "name_discord",
+		link_twitter: "link_twitter",
+		link_linkedin: "link_linkedin",
+		link_facebook: "link_facebook",
+		min_hour_rate: "min_hour_rate",
+		hours_per_day: "hours_per_day"
+	};
+
+	Object.keys(columnMap).forEach((key) => {
+		if (req.body[key]) {
+			query += `, ${columnMap[key]}`;
+			values.push(req.body[key]);
 		}
-	);
+	});
+
+	query += ") VALUES(?";
+	for (let i = 1; i < values.length; i++) {
+		query += ",?";
+	}
+	query += ")";
+	console.log(query);
+	connection.query(query, values, function (err, result) {
+		if (err) return next(new AppError(err, 500));
+		res.status(201).json({
+			status: "success",
+			message: "gamer created!",
+			data: result
+		});
+	});
 };
 
+//function to get a gamer's id using its username
 exports.getGamerId = (req, res, next) => {
 	if (!req.body.username)
 		return next(new AppError("No gamer username found", 404));
@@ -52,6 +88,7 @@ exports.getGamerId = (req, res, next) => {
 	);
 };
 
+//function to get a gamer using its id
 exports.getGamer = (req, res, next) => {
 	if (!req.params.id) {
 		return next(new AppError("No gamer id found", 404));
@@ -70,6 +107,7 @@ exports.getGamer = (req, res, next) => {
 	);
 };
 
+//function to check if a user exists using username return true if exists
 exports.checkUserExists = (username) => {
 	let query = "SELECT 1 FROM gamers WHERE LOWER(username) = LOWER(?)";
 	connection.query(query, [username], function (error, results) {
@@ -82,10 +120,13 @@ exports.checkUserExists = (username) => {
 	});
 };
 
+//function to update a gamer
 exports.updateGamer = (req, res, next) => {
 	if (!req.params.id || !req.body || Object.keys(req.body).length === 0) {
 		return next(new AppError("No gamer id or form data found", 404));
 	}
+	if (this.checkUserExists(req.body.username))
+		return next(new AppError("Username already exists", 400));
 	if (
 		req.body.profile_type &&
 		req.body.profile_type !== "Gamer" &&
@@ -93,76 +134,44 @@ exports.updateGamer = (req, res, next) => {
 		req.body.profile_type !== "Guild Manager"
 	)
 		return next(new AppError("Incorrect type of profile", 400));
+	if (req.body.min_hour_rate && req.body.min_hour_rate <= 0)
+		return next(new AppError("Wrong minimum hour rate value", 400));
+	if (req.body.hours_per_day >= 24 && req.body.hours_per_day <= 0)
+		return next(new AppError("Wrong hours_per_day hour rate value", 400));
 
+	//initiate query and values
 	let query = "UPDATE gamers SET ";
 	let values = [];
-	if (req.body.username) {
-		if (!this.checkUserExists(req.body.username)) {
-			query += "username = ?, ";
-			values.push(req.body.username);
-		} else {
-			return next(new AppError("Username already used", 400));
+
+	const columnMap = {
+		username: "username",
+		profile_type: "profile_type",
+		birthdate: "birthdate",
+		description: "description",
+		location: "location",
+		birthdate: "birthdate",
+		description: "description",
+		name_discord: "name_discord",
+		link_twitter: "link_twitter",
+		link_linkedin: "link_linkedin",
+		link_facebook: "link_facebook",
+		min_hour_rate: "min_hour_rate",
+		hours_per_day: "hours_per_day",
+		total_earned: "total_earned"
+	};
+
+	Object.keys(columnMap).forEach((key) => {
+		if (req.body[key]) {
+			query += `${columnMap[key]} = ?, `;
+			values.push(req.body[key]);
 		}
-	}
-	if (req.body.profile_type) {
-		query += "profile_type = ?, ";
-		values.push(req.body.profile_type);
-	}
-	if (req.body.birthdate) {
-		query += "birthdate = ?, ";
-		values.push(req.body.birthdate);
-	}
-	if (req.body.description) {
-		query += "description = ?, ";
-		values.push(req.body.description);
-	}
-	if (req.body.location) {
-		query += "location = ?, ";
-		values.push(req.body.location);
-	}
-	if (req.body.name_discord) {
-		query += "name_discord = ?, ";
-		values.push(req.body.name_discord);
-	}
-	if (req.body.link_twitter) {
-		query += "link_twitter = ?, ";
-		values.push(req.body.link_twitter);
-	}
-	if (req.body.link_linkedin) {
-		query += "link_linkedin = ?, ";
-		values.push(req.body.link_linkedin);
-	}
-	if (req.body.link_facebook) {
-		query += "link_facebook = ?, ";
-		values.push(req.body.link_facebook);
-	}
-	if (req.body.min_hour_rate) {
-		if (req.body.min_hour_rate > 0) {
-			query += "min_hour_rate = ?, ";
-			values.push(req.body.min_hour_rate);
-		} else {
-			return next(new AppError("Wrong minimum hour rate value", 400));
-		}
-	}
-	if (req.body.hours_per_day) {
-		if (req.body.hours_per_day < 24 && req.body.hours_per_day > 0) {
-			query += "hours_per_day = ?, ";
-			values.push(req.body.hours_per_day);
-		} else {
-			return next(new AppError("Wrong hours per day value", 400));
-		}
-	}
-	if (req.body.total_earned) {
-		if (req.body.total_earned > 0) {
-			query += "total_earned = ?, ";
-			values.push(req.body.total_earned);
-		} else {
-			return next(new AppError("Wrong total earned value", 400));
-		}
-	}
+	});
+
 	query = query.slice(0, -2); // Removing the last comma and space
 	query += " WHERE id = ?";
 	values.push(req.params.id);
+	console.log(query);
+
 	connection.query(query, values, function (err, data, fields) {
 		if (err) return next(new AppError(err, 500));
 		res.status(200).json({
@@ -172,6 +181,7 @@ exports.updateGamer = (req, res, next) => {
 	});
 };
 
+//function to delete a gamer using its id
 exports.deleteGamer = (req, res, next) => {
 	if (!req.params.id) {
 		return next(new AppError("No gamer id found", 404));
