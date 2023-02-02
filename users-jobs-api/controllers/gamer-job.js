19e7c01a8062c8083c2a6fe742c9b584624dc538
CalvinC44 <calel44jke@gmail.com>
2023-02-02 23:16:07 +0100

    update : getTotalEarned

--- users-jobs-api/controllers/gamer-job.js
+++ users-jobs-api/controllers/gamer-job.js
@@ -1,4 +1,43 @@
 //function to get total_earned, if null does the request and save it
+exports.getTotalEarned = (req, res, next) => {
+	if (!req.params.id) {
+		return next(new AppError("No gamer id found", 404));
+	}
+	connection.query(
+		"SELECT total_earned FROM gamers WHERE id = ?",
+		[req.params.id],
+		function (err, data, fields) {
+			if (err) return next(new AppError(err, 500));
+			if (data[0].total_earned === null) {
+				connection.query(
+					"SELECT SUM(amount) AS total_earned FROM jobs WHERE gamer_id = ?",
+					[req.params.id],
+					function (err, data, fields) {
+						if (err) return next(new AppError(err, 500));
+						connection.query(
+							"UPDATE gamers SET total_earned = ? WHERE id = ?",
+							[data[0].total_earned, req.params.id],
+							function (err, data, fields) {
+								if (err) return next(new AppError(err, 500));
+								res.status(200).json({
+									status: "success",
+									length: data?.length,
+									data: data
+								});
+							}
+						);
+					}
+				);
+			} else {
+				res.status(200).json({
+					status: "success",
+					length: data?.length,
+					data: data
+				});
+			}
+		}
+	);
+};
 
 //function to make a gamer apply for a job
 
