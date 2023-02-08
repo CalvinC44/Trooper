140010a2102a4c1ac6bbc32a87f9420ad235bb6b
CalvinC44 <calel44jke@gmail.com>
2023-02-08 15:08:32 +0100

    update: updateJob done

--- users-jobs-api/controllers/job.js
+++ users-jobs-api/controllers/job.js
@@ -27,30 +27,25 @@ exports.getJob = async (req, res, next) => {
 		if (!req.params.id) {
 			return next(new AppError("No job id found", 404));
 		}
-		connection.query(
-			`
-			SELECT jobs.*, 
-			GROUP_CONCAT(roles.role_name) as roles, 
-			GROUP_CONCAT(gamers_jobs_applicants.gamer_id) as applicants, 
-			GROUP_CONCAT(gamers_jobs_asked_gamers.gamer_id) as asked_gamers 
-			FROM jobs LEFT JOIN jobs_roles ON jobs.id = jobs_roles.job_id 
-			LEFT JOIN roles ON jobs_roles.role_id = roles.id 
-			LEFT JOIN gamers_jobs_applicants ON jobs.id = gamers_jobs_applicants.job_id 
-			AND gamers_jobs_applicants.application_state = 'Approved' 
-			LEFT JOIN gamers_jobs_asked_gamers ON jobs.id = gamers_jobs_asked_gamers.job_id 
-			AND gamers_jobs_asked_gamers.recruitment_state = 'Approved' 
-			WHERE jobs.id = ? 
-			GROUP BY jobs.id`,
-			[req.params.id],
-			function (err, data, fields) {
-				if (err) return next(new AppError(err, 500));
-				res.status(200).json({
-					status: "success",
-					length: data?.length,
-					data: data
-				});
-			}
-		);
+		const query = `SELECT jobs.*, 
+					GROUP_CONCAT(roles.role_name) as roles, 
+					GROUP_CONCAT(gamers_jobs_applicants.gamer_id) as applicants, 
+					GROUP_CONCAT(gamers_jobs_asked_gamers.gamer_id) as asked_gamers 
+					FROM jobs LEFT JOIN jobs_roles ON jobs.id = jobs_roles.job_id 
+					LEFT JOIN roles ON jobs_roles.role_id = roles.id 
+					LEFT JOIN gamers_jobs_applicants ON jobs.id = gamers_jobs_applicants.job_id 
+					AND gamers_jobs_applicants.application_state = 'Approved' 
+					LEFT JOIN gamers_jobs_asked_gamers ON jobs.id = gamers_jobs_asked_gamers.job_id 
+					AND gamers_jobs_asked_gamers.recruitment_state = 'Approved' 
+					WHERE jobs.id = ? GROUP BY jobs.id`;
+		connection.query(query, [req.params.id], function (err, data, fields) {
+			if (err) return next(new AppError(err, 500));
+			res.status(200).json({
+				status: "success",
+				length: data?.length,
+				data: data
+			});
+		});
 	} catch (err) {
 		return next(new AppError(err, 500));
 	}
@@ -59,6 +54,9 @@ exports.getJob = async (req, res, next) => {
 //function to create a job, require a job_name and the recruiter_id, others are optional, job_state is set to Available by default
 exports.createJob = async (req, res, next) => {
 	try {
+		if (!req.body.recruiter_id) {
+			return next(new AppError("No recruiter_id found", 404));
+		}
 		if (!req.body) return next(new AppError("No form data found", 404));
 		if (!req.body.job_name) return next(new AppError("No job_name found"));
 		if (!req.body.recruiter_id)
@@ -108,7 +106,6 @@ exports.createJob = async (req, res, next) => {
 					});
 				}
 
-				console.log(job_id);
 				if (req.body.roles) {
 					const roleValues = req.body.roles.map((role_id) => [job_id, role_id]);
 					const roleQuery = "INSERT INTO jobs_roles (job_id, role_id) VALUES ?";
@@ -126,6 +123,7 @@ exports.createJob = async (req, res, next) => {
 							}
 							res.status(201).json({
 								status: "success",
+								message: "Job created successfully",
 								data: {
 									job_id: job_id
 								}
@@ -141,6 +139,7 @@ exports.createJob = async (req, res, next) => {
 						}
 						res.status(201).json({
 							status: "success",
+							message: "Job created successfully",
 							data: {
 								job_id: job_id
 							}
@@ -161,34 +160,157 @@ exports.updateJob = async (req, res, next) => {
 			return next(new AppError("No job id found", 404));
 		}
 		if (!req.body) return next(new AppError("No form data found", 404));
-		let query = "UPDATE jobs SET ";
-		let values = [];
-		if (req.body.job_name) {
-			query += "job_name = ?, ";
-			values.push(req.body.job_name);
-		}
-		if (req.body.birthdate) {
-			query += "birthdate = ?, ";
-			values.push(req.body.birthdate);
-		}
-		if (req.body.description) {
-			query += "description = ?, ";
-			values.push(req.body.description);
-		}
-		if (req.body.location) {
-			query += "location = ?, ";
-			values.push(req.body.location);
-		}
-		query = query.slice(0, -2); // Removing the last comma and space
-		query += " WHERE id = ?";
-		values.push(req.params.id);
-		connection.query(query, values, function (err, data, fields) {
-			if (err) return next(new AppError(err, 500));
-			res.status(200).json({
-				status: "success",
-				message: "job updated!"
+
+		//query to update the job details, if there are any
+		if (
+			req.body.job_name ||
+			req.body.short_description ||
+			req.body.description ||
+			req.body.game_id ||
+			req.body.payment_amount ||
+			req.body.duration ||
+			req.body.chosen_gamer_id
+		) {
+			let query = "UPDATE jobs SET ";
+			let values = [];
+
+			const columnMap = {
+				job_name: "job_name",
+				short_description: "short_description",
+				description: "description",
+				game_id: "game_id",
+				payment_amount: "payment_amount",
+				duration: "duration",
+				chosen_gamer_id: "chosen_gamer_id"
+			};
+
+			Object.keys(columnMap).forEach((key) => {
+				if (req.body[key]) {
+					query += `${columnMap[key]} = ?, `;
+					values.push(req.body[key]);
+				}
 			});
-		});
+
+			if (req.body.chosen_gamer_id) {
+				query += `job_state = ?, `;
+				values.push("In progress");
+			}
+
+			query = query.slice(0, -2);
+			query += " WHERE id=?";
+			values.push(req.params.id);
+
+			connection.beginTransaction(function (err) {
+				if (err) {
+					return next(new AppError(err, 500));
+				}
+
+				//query to update the job details, if there are any
+				connection.query(query, values, function (err, result) {
+					if (err) {
+						return connection.rollback(function () {
+							return next(new AppError(err, 500));
+						});
+					}
+
+					if (req.body.roles) {
+						const roleValues = req.body.roles.map((role_id) => [
+							req.params.id,
+							role_id
+						]);
+						const roleQuery =
+							"INSERT INTO jobs_roles (job_id, role_id) VALUES ?";
+
+						//query to delete the roles of the job
+						connection.query(
+							"DELETE FROM jobs_roles WHERE job_id=?",
+							req.params.id,
+							function (err, result) {
+								if (err) {
+									console.log("DELETE roles");
+									return connection.rollback(function () {
+										return next(new AppError(err, 500));
+									});
+								}
+							}
+						);
+
+						//query to update the roles for the job
+						connection.query(roleQuery, [roleValues], function (err, result) {
+							if (err) {
+								return connection.rollback(function () {
+									return next(new AppError(err, 500));
+								});
+							}
+							connection.commit(function (err) {
+								if (err) {
+									return connection.rollback(function () {
+										return next(new AppError(err, 500));
+									});
+								}
+								res.status(200).json({
+									status: "success",
+									message: "Job updated successfully"
+								});
+							});
+						});
+					} else {
+						connection.commit(function (err) {
+							if (err) {
+								return connection.rollback(function () {
+									return next(new AppError(err, 500));
+								});
+							}
+							res.status(200).json({
+								status: "success",
+								message: "Job updated successfully"
+							});
+						});
+					}
+				});
+			});
+			//else if no job details, update the roles
+		} else {
+			const roleValues = req.body.roles.map((role_id) => [
+				req.params.id,
+				role_id
+			]);
+			const roleQuery = "INSERT INTO jobs_roles (job_id, role_id) VALUES ?";
+
+			//query to delete the roles of the job
+			connection.query(
+				"DELETE FROM jobs_roles WHERE job_id=?",
+				req.params.id,
+				function (err, result) {
+					if (err) {
+						console.log("DELETE roles");
+						return connection.rollback(function () {
+							return next(new AppError(err, 500));
+						});
+					}
+				}
+			);
+
+			//query to update the roles for the job
+			connection.query(roleQuery, [roleValues], function (err, result) {
+				if (err) {
+					return connection.rollback(function () {
+						return next(new AppError(err, 500));
+					});
+				}
+				connection.commit(function (err) {
+					if (err) {
+						return connection.rollback(function () {
+							return next(new AppError(err, 500));
+						});
+					}
+					res.status(200).json({
+						status: "success",
+						message: "Job updated successfully"
+					});
+				});
+			});
+		}
 	} catch (err) {
 		return next(new AppError(err, 500));
 	}
