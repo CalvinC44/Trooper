4658a9011246809a8a4cbdd7c7e0d5cdc183400e
CalvinC44 <calel44jke@gmail.com>
2023-02-16 22:25:17 +0100

    fix: both get dont only get done jobs, table gamers_jobs_applications name changed

--- users-jobs-api/controllers/job.js
+++ users-jobs-api/controllers/job.js
@@ -112,15 +112,13 @@ exports.getAllJobs = async (req, res, next) => {
 		connection.query(
 			`SELECT jobs.*, 
 			GROUP_CONCAT(roles.role_name) as roles, 
-			GROUP_CONCAT(gamers_jobs_applicants.gamer_id) as applicants, 
+			GROUP_CONCAT(gamers_jobs_applications.gamer_id) as applicants, 
 			GROUP_CONCAT(gamers_jobs_asked.gamer_id) as asked_gamers 
 			FROM jobs 
 			LEFT JOIN jobs_roles ON jobs.job_id = jobs_roles.job_id 
 			LEFT JOIN roles ON jobs_roles.role_id = roles.id 
-			LEFT JOIN gamers_jobs_applicants ON jobs.job_id = gamers_jobs_applicants.job_id 
-			AND gamers_jobs_applicants.application_state = 'Approved' 
-			LEFT JOIN gamers_jobs_asked ON jobs.job_id = gamers_jobs_asked.job_id 
-			AND gamers_jobs_asked.recruitment_state = 'Approved' 
+			LEFT JOIN gamers_jobs_applications ON jobs.job_id = gamers_jobs_applications.job_id
+			LEFT JOIN gamers_jobs_asked ON jobs.job_id = gamers_jobs_asked.job_id
 			GROUP BY jobs.job_id`,
 			function (err, data, fields) {
 				if (err) return next(new AppError(err));
@@ -144,14 +142,12 @@ exports.getJob = async (req, res, next) => {
 		}
 		const query = `SELECT jobs.*, 
 					GROUP_CONCAT(roles.role_name) as roles, 
-					GROUP_CONCAT(gamers_jobs_applicants.gamer_id) as applicants, 
+					GROUP_CONCAT(gamers_jobs_applications.gamer_id) as applicants, 
 					GROUP_CONCAT(gamers_jobs_asked.gamer_id) as asked_gamers 
 					FROM jobs LEFT JOIN jobs_roles ON jobs.job_id = jobs_roles.job_id 
 					LEFT JOIN roles ON jobs_roles.role_id = roles.id 
-					LEFT JOIN gamers_jobs_applicants ON jobs.job_id = gamers_jobs_applicants.job_id 
-					AND gamers_jobs_applicants.application_state = 'Approved' 
-					LEFT JOIN gamers_jobs_asked ON jobs.job_id = gamers_jobs_asked.job_id 
-					AND gamers_jobs_asked.recruitment_state = 'Approved' 
+					LEFT JOIN gamers_jobs_applications ON jobs.job_id = gamers_jobs_applications.job_id
+					LEFT JOIN gamers_jobs_asked ON jobs.job_id = gamers_jobs_asked.job_id
 					WHERE jobs.job_id = ? GROUP BY jobs.job_id`;
 		connection.query(query, [req.params.job_id], function (err, data, fields) {
 			if (err) return next(new AppError(err, 500));
